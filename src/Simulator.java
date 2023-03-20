a22545fdb2febf89879d1a4032e0ad393e30f3c3
cccjjjjjj <113432966+cccjjjjjj@users.noreply.github.com>
2023-03-20 20:58:30 +0800

    added methods

--- src/Simulator.java
+++ src/Simulator.java
@@ -35,11 +35,83 @@ public class Simulator {
                 parts.setArrivalTimeInService();
             }
         }
-        // numOfParts
-        // partsThatPassQueue
-        // totalWaitingTime
-        // maximumWaitingTime
-        // totalTimeInSystem
-        // maximumTimeInSystem
+        public int calculateTime() {
+
+        }
+
+        public String checkEventType() {
+
+        }
+
+        public int calculateVariableQ() {
+
+        }
+
+        public int calculateVariableB() {
+
+        }
+
+        public int calculateArrivalTimeInQueue() {
+
+        }
+
+        public int calculateArrivalTimeInService() {
+
+        }
+
+        /*
+                FOR STATISTICAL ACCUMULATORS
+                     P = numberOfParts
+                     N = partsThatPassQueue
+                     ΣWQ = totalWaitingTime
+                     WQ* = maxWaitingTime
+                     ΣTS = totalTimeInSystem
+                     TS* = maxTimeInSystem
+                     ∫Q = integralQ
+                     Q* = maxQ
+                     ∫B = integralB
+                     B* = maxB
+         */
+
+        public int calculateNumberOfParts() {
+
+        }
+
+        public int calculatePartsThatPassQueue() {
+
+        }
+
+        public int calculateTotalWaitingTime() {
+
+        }
+
+        public int calculateMaxWaitingTime() {
+
+        }
+
+        public int calculateTotalTimeInSystem() {
+
+        }
+
+        public int calculateMaxTimeInSystem() {
+
+        }
+
+        public int calculateIntegralQ() {
+
+        }
+
+        public int calculateMaxQ() {
+
+        }
+
+        public int calculateIntegralB() {
+
+        }
+
+        public int calculateMaxB() {
+
+        }
+
     }
 }
