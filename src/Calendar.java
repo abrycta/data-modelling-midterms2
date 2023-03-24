30cf77be30af1dce17929f796098d5ab6970aa99
Mebreda <113490912+Mebreda@users.noreply.github.com>
2023-03-24 13:59:48 +0800

    Delete Calendar.java

--- src/Calendar.java
+++
@@ -1,7 +0,0 @@
-import java.util.List;
-
-public class Calendar {
-    // abstraction for the entire table in the module
-    // contains Events as rows
-    List<Event> events;  // = new Arraylist();
-}
