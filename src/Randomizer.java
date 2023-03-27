edaec74f2d46c9966a38fe98b1f9af40470a0768
Jadesuu <90382683+Jadesuu@users.noreply.github.com>
2023-03-28 07:22:28 +0800

    Fixed randomizer.java

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -1,24 +1,41 @@
 import java.util.Random;
 
 public class Randomizer {
+
     private static final Random RAND = new Random();
 
-    public static double lookupInterArrivalTime() {
-        double mean = 0.0;
-        double standardDeviation = 1.0;
+    public static int lookupInterArrivalTime() {
+        double mean = 10.0;
+        double standardDeviation = 2.0;
         int min = 1;
         int max = 8;
-        double randomValue = (mean + RAND.nextGaussian() * standardDeviation);
+        int randomValue = (int) (mean + RAND.nextGaussian() * standardDeviation);
         return Math.max(min, Math.min(max, randomValue));
     }
 
-    public static double lookUpServiceTime() {
-        double mean = 0.0;
-        double standardDeviation = 1.0;
+    public static int lookUpServiceTime() {
+        double mean = 8.0;
+        double standardDeviation = 2.0;
         int min = 1;
         int max = 5;
-        double randomValue =  (mean + RAND.nextGaussian() * standardDeviation);
+        int randomValue = (int) (mean + RAND.nextGaussian() * standardDeviation);
         return Math.max(min, Math.min(max, randomValue));
     }
+
+    //test
+    public static void main(String[] args) {
+        System.out.println("inter arrival time");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(lookupInterArrivalTime());
+        }
+
+        System.out.println("service time");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(lookUpServiceTime());
+        }
+    }
 }
+
+
+
 
